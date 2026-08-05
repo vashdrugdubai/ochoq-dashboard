dcff3823ab738cf0aec44f621120759a8d0aa26c
Aibek Aliev <alievuae@icloud.com>
2026-08-05 19:09:02 +0400

    Кабинет ресторана: сборка 2026-08-05 19:09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "726787411" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3367882382" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
